0b91c549030852f31aea1f013e22d1ae03ba0c7f
Kristian Quirapas <kmquirapas@gmail.com>
2023-11-21 11:21:02 +0800

    first commit

--- src/Pledge.sol
+++ src/Pledge.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Counter {
+contract Pledge is Ownable {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
---
+++ test/Pledge.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Pledge} from "../src/Pledge.sol";
+
+contract PledgeTest is Test {
+
+    function setUp() public {
+    }
+
+    function test_Increment() public {
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+    }
+}
